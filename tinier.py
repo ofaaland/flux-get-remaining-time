561c0adcbe697dc2d06dc7e406c881410406dfc6
Olaf Faaland <faaland1@llnl.gov>
2022-12-16 22:10:02 -0800

    switch between code blocks with if
    that way I can experiment without creating diffs

--- tinier.py
+++ tinier.py
@@ -7,17 +7,19 @@ import flux.uri
 from flux.job import JobID, JobInfo, JobInfoFormat, JobList
 from flux.util import Tree
 
-#handle = flux.Flux()
-##size = handle.attr_get("size")
-#jobid = JobID(handle.attr_get("jobid"))
-#parent = flux.Flux(handle.attr_get("parent-uri"))
-#info = JobList(parent, ids=[jobid]).fetch_jobs().get_jobs()[0]
-#print(f'parent is {parent}')
-#print(f'info is {info}')
-
-# my code
-handle = flux.Flux()
-print(f'handle is {handle}')
-parenturi = handle.attr_get('parent-uri')
-print(f'parenturi is {parenturi}')
-parent = handle.Flux(parenturi)
+if True:
+  # code from flux-pstree.py
+  handle = flux.Flux()
+  #size = handle.attr_get("size")
+  jobid = JobID(handle.attr_get("jobid"))
+  parent = flux.Flux(handle.attr_get("parent-uri"))
+  info = JobList(parent, ids=[jobid]).fetch_jobs().get_jobs()[0]
+  print(f'parent is {parent}')
+  print(f'info is {info}')
+else:
+  # my code
+  handle = flux.Flux()
+  print(f'handle is {handle}')
+  parenturi = handle.attr_get('parent-uri')
+  print(f'parenturi is {parenturi}')
+  parent = handle.Flux(parenturi)
