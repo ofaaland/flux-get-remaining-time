f134225e7a2b12365f6dee4c7bdacb8d3600ed8a
Olaf Faaland <faaland1@llnl.gov>
2022-12-16 22:03:07 -0800

    add sample from flux-pstree.py

---
+++ tinier.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import flux
+
+# from flux-pstree.py
+import flux.uri
+from flux.job import JobID, JobInfo, JobInfoFormat, JobList
+from flux.util import Tree
+
+handle = flux.Flux()
+#size = handle.attr_get("size")
+jobid = JobID(handle.attr_get("jobid"))
+parent = flux.Flux(handle.attr_get("parent-uri"))
+info = JobList(parent, ids=[jobid]).fetch_jobs().get_jobs()[0]
+print(f'parent is {parent}')
+print(f'info is {info}')
+
+# my code
+#handle = flux.Flux()
+#print(f'handle is {handle}')
+#parenturi = handle.attr_get('parent-uri')
+#print(f'parenturi is {parenturi}')
+#parent = handle.Flux(parenturi)
